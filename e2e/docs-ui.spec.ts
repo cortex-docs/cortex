94944b5034454de835adc17e2cc1b9075eef6670
Nick Chisiu <8492343+nickchisiu@users.noreply.github.com>
2026-08-25 10:35:10 +0300

    fix(docs): increase badge separation

--- e2e/docs-ui.spec.ts
+++ e2e/docs-ui.spec.ts
@@ -60,7 +60,7 @@ test.describe('Docs UI', () => {
     ]);
     expect(footerBox).not.toBeNull();
     expect(paginationBox).not.toBeNull();
-    expect(Math.round(footerBox!.y - (paginationBox!.y + paginationBox!.height))).toBe(32);
+    expect(Math.round(footerBox!.y - (paginationBox!.y + paginationBox!.height))).toBe(48);
   });
 
   test('shows authentication info', async ({ page }) => {
